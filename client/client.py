44d87af0a1f7028ad96a2849d97be57d8849277c
Mahetem Moges <mmoges3@gatech.edu>
2018-12-09 14:56:09 -0500

    IMU 1.3

--- client/client.py
+++ client/client.py
@@ -10,10 +10,11 @@ s.connect((HOST, PORT))
 for x in range(0, 100):
     # make sure the data is sent in 5 digit length
     print("Step 1")
-    arr1 = x
-    arr2 = 2 * x
-    someVar = 300 - x
-    data = json.dumps({"r": arr1, "p": arr2, "y": someVar})
+    arr1 = format(1.22*x,'3.2f')
+    arr2 = format(2.22 * x,'3.2f')
+    someVar = format(300.22 - x,'3.2f')
+
+    data = json.dumps({"r": float(arr1), "p": float(arr2), "y": float(someVar)})
     s.send(data.encode())
     print("Step 2")
     print(str(s.recv(1000)))
