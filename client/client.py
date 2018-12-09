d43725bd14638688edd85a19f6b62385c3043a18
Mahetem Moges <mmoges3@gatech.edu>
2018-12-08 20:53:12 -0500

    IMU integration 1.0

--- client/client.py
+++ client/client.py
@@ -24,6 +24,9 @@
 import logging
 import sys
 import time
+import socket
+# import sys
+import json
 
 from Adafruit_BNO055 import BNO055
 
@@ -63,19 +66,35 @@ print('Magnetometer ID:    0x{0:02X}'.format(mag))
 print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
 
 print('Reading BNO055 data, press Ctrl-C to quit...')
+
+# Client start
+HOST, PORT = "128.61.25.113", 50007
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+# Client end
+
 while True:
     # Read the Euler angles for heading, roll, pitch (all in degrees).
     yaw, roll, pitch = bno.read_euler()
     # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
     sys, gyro, accel, mag = bno.get_calibration_status()
-    # Print everything out.
+
+    # Client start
+    data = json.dumps({"r": roll, "p": pitch, "y": yaw})
+    s.send(data.encode())
+    print(str(s.recv(1000)))
+    # Client end
 
     yaw = format(yaw, '3.2f')
     roll = format(roll, '3.2f')
     pitch = format(pitch, '3.2f')
 
+    # yawSize = yaw.size()
+    # rollSize = roll.size()
+    # pitchSize = pitch.size()
+
     print('yaw={0} Roll={1} Pitch={2}\t'.format(
-          yaw, roll, pitch))
+          yaw, roll, pitch, ))
     # Other values you can optionally read:
     # Orientation as a quaternion:
     #x,y,z,w = bno.read_quaterion()
