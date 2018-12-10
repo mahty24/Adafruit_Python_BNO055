eb26cf9cc5c749c7df1cd653090a3afb1d585374
Mahetem Moges <mmoges3@gatech.edu>
2018-12-10 18:11:21 -0500

    IMU v 1.13

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -62,15 +62,33 @@ print('Accelerometer ID:   0x{0:02X}'.format(accel))
 print('Magnetometer ID:    0x{0:02X}'.format(mag))
 print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
 
+yawRef = 0
+pitchRef = 0
+count = 0
+
 print('Reading BNO055 data, press Ctrl-C to quit...')
 while True:
     # Read the Euler angles for heading, roll, pitch (all in degrees).
-    heading, roll, pitch = bno.read_euler()
+    yaw, roll, pitch = bno.read_euler()
     # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
     sys, gyro, accel, mag = bno.get_calibration_status()
     # Print everything out.
-    print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
-          heading, roll, pitch, sys, gyro, accel, mag))
+    if count == 0:
+        yawRef = yaw
+        pitchRef = pitch
+        count = 1
+
+    if yawRef > 180:
+        yaw = yaw - yawRef
+        if yaw < 180:
+            yaw = 360 + yaw
+    else:
+        yaw = yaw - yawRef
+        if yaw > 180:
+            yaw = 360 - yaw
+
+    print('yaw={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
+          yaw, roll, pitch, sys, gyro, accel, mag))
     # Other values you can optionally read:
     # Orientation as a quaternion:
     #x,y,z,w = bno.read_quaterion()
